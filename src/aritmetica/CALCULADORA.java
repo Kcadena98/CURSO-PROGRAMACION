6a3f62228b3a723590c35ff163ace5923ad83497
Muñecas <Muecas@DESKTOP-SAGLVHD>
2019-04-20 13:17:01 -0500

    ARITMETICA

---
+++ src/aritmetica/CALCULADORA.java
@@ -0,0 +1,58 @@
+package aritmetica;
+
+import java.util.Scanner;
+
+public class CALCULADORA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		Scanner entrada = new Scanner (System.in); // datos que ordenamos a partir del teclado del computador 
+		
+		
+		int numero1, numero2, opcion;
+		System.out.println("Ingrese Numero1: ");
+		numero1= entrada.nextInt();
+		System.out.println("Ingrese Numero2: ");
+		numero2= entrada.nextInt();
+		
+		do {
+				System.out.println("1.- Sumar.  "); 
+				System.out.println("2.- Restar.  ");
+				System.out.println("3.- Multiplicar.");
+				System.out.println("4.- Dividir.");
+				System.out.println("5.- salir.");
+				System.out.println("\n Ingrese una opcion: "); 
+
+		opcion = entrada.nextInt(); 
+		} while (opcion < 1 || opcion > 5 ); 
+		switch (opcion) {
+		
+		case 1: 
+			System.out.println(numero1 + numero2);
+			break;
+		case 2: 
+			System.out.println(numero1 - numero2);
+			break;
+		case 3:
+			System.out.println(numero1 * numero2);
+			break;
+		case 4: 
+			System.out.println(numero1 / numero2);
+			break;
+		case 5:
+			System.exit(0);
+			break;
+			
+		
+			default: System.out.println("caso no encontrado"); 
+			
+		}
+	}
+}
+		
+	
+
+
+
+
---
+++ src/aritmetica/MAXIMODERESNUMEROS.java
@@ -0,0 +1,39 @@
+package aritmetica;
+
+import java.util.Scanner;
+
+public class MAXIMODERESNUMEROS {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner (System.in); // esta opcion es util para leer los datos que ordenamos del teclado
+		
+		int numero1, numero2, numero3; // variables para almacenar los datos
+		
+		
+		System.out.println("ingrese el numero");
+		numero1 = entrada.nextInt(); 
+		
+		System.out.println("ingrese el numero");
+		numero2 = entrada.nextInt(); 
+		
+		
+		System.out.println("ingrese el numero");
+		numero3 = entrada.nextInt(); 
+		
+		if(numero1 > numero2) // si el numero 1 es mayor a numero 2
+			if (numero1 > numero3) // si el numero 1 es no es mayor al numero 3 
+				System.out.println("el mayor es" + numero1); // imprima que el mayor es el numero 1
+			else  // si no es mayor al numero 3 
+				System.out.println("el mayor es " + numero3); // imprima que el mayor sea el numero 3
+		
+		else if (numero2 > numero3) // en caso de que sea falso se pregunta una condicion 
+			System.out.println("el mayor es " + numero2);
+		else 
+			System.out.println("el mayor es " + numero3);
+		
+			
+	}
+}
+
+
